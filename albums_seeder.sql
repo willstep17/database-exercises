0086e41a252663c7412d83bdad1bbf29a130ec88
William Stephens <william.a.stephens17@gmail.com>
2020-02-20 09:18:32 -0600

    Finished SQL SELECT exercise.

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,13 +1,13 @@
 USE codeup_test_db;
 INSERT INTO albums (artist, name, release_date, genre, sales)
 VALUES ('Michael Jackson', 'Thriller', '1982', 'Pop, rock, R&B', '47.3'),
-('AC/DC', 'Back in Black', '1980', 'Hard rock', '26.4')
+('AC/DC', 'Back in Black', '1980', 'Hard rock', '26.4'),
 ('Meat Loaf', 'Bat Out of Hell', '1977', 'Hard rock, glam rock, heavy metal', '21.7'),
 ('Pink Floyd', 'The Dark Side of the Moon', '1973', 'Progressive rock', '24.2'),
 ('Whitney Houston / Various artists', 'The Bodyguard', '1992', 'R&B, soul, pop, soundtrack', '28.4'),
 ('Eagles', 'Their Greatest Hits (1971–1975)', '1976', 'Country rock, soft rock, folk rock', '41.2'),
 ('Bee Gees / Various artists', 'Saturday Night Fever', '1977', 'Disco', ' 21.6'),
-('Fleetwood Mac', 'Rumours', '1977', 'Soft rock' '27.9'),
+('Fleetwood Mac', 'Rumours', '1977', 'Soft rock', '27.9'),
 ('Shania Twain', 'Come On Over', '1997', 'Country, pop', '29.6'),
 ('Various artists', 'Grease: The Original Soundtrack from the Motion Picture', '1978', 'Rock and roll', '14.4'),
 ('Led Zeppelin', 'Led Zeppelin IV', '1971', 'Hard rock, heavy metal, folk rock', '29.0'),
---
+++ select_exercises.sql
@@ -0,0 +1,7 @@
+USE codeup_test_db;
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT name FROM albums WHERE 1989 < release_date && release_date < 2000;
+SELECT name FROM albums WHERE sales > 20;
+SELECT name FROM albums WHERE genre = 'Rock';
