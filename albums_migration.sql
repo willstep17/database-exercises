10708985a3dd7239f3d169caa7cc16814667375f
William Stephens <william.a.stephens17@gmail.com>
2020-02-19 19:39:45 -0600

    Set up albums table

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100),
+    name  VARCHAR(100),
+    release_date INT,
+    sales DECIMAL,
+    genre VARCHAR(200),
+    PRIMARY KEY (id)
+);
