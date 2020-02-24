df204d450fc83adf252aacfeea6db65c9af5ce1b
William Stephens <william.a.stephens17@gmail.com>
2020-02-24 16:52:00 -0600

    Creating quiz database

---
+++ quiz_creation.sql
@@ -0,0 +1,31 @@
+USE quiz;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    email VARCHAR(320),
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT uc_email UNIQUE (email)
+);
+
+CREATE TABLE IF NOT EXISTS ads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    title VARCHAR(100),
+    content TEXT NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS ads_categories (
+    ad_id INTEGER UNSIGNED NOT NULL,
+    category_id INTEGER UNSIGNED NOT NULL,
+    FOREIGN KEY (ad_id) REFERENCES ads(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
