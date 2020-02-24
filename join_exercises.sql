27a1fa84c3075386e88d5b29cba64b42fa29ff46
William Stephens <william.a.stephens17@gmail.com>
2020-02-24 10:44:36 -0600

    JOIN bonus

--- join_exercises.sql
+++ join_exercises.sql
@@ -45,19 +45,44 @@ WHERE dm.to_date = '9999-01-01' AND e.gender = 'F'
 ORDER BY d.dept_name;
 
 /* Find the current titles of employees currently working in the Customer Service department. */
-SELECT t.title AS Title, count(t.title)
-FROM employees AS e
-JOIN titles AS t
-ON e.emp_no = t.emp_no
+SELECT t.title AS Title, count(t.title) AS 'Count'
+FROM titles AS t
 JOIN dept_emp AS de
 ON t.emp_no = de.emp_no
 JOIN departments AS d
 ON de.dept_no = d.dept_no
 WHERE d.dept_name = 'Customer Service'
+AND t.to_date = '9999-01-01'
 GROUP BY t.title;
 
+/* Find the current salary of all current managers. */
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Name', s.salary AS 'Salary'
+FROM employees AS E
+JOIN dept_manager AS dm
+ON e.emp_no = dm.emp_no
+JOIN salaries AS s
+ON dm.emp_no = s.emp_no
+JOIN departments AS d
+ON d.dept_no = dm.dept_no
+WHERE s.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
+ORDER BY d.dept_name;
 
-SELECT * FROM employees WHERE emp_no = 110039;
+/* Find the names of all current employees, their department name, and their current manager's name. */
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name',
+d.dept_name AS 'Department Name',
+CONCAT(e.first_name, e.last_name) AS 'Manager Name'
+FROM employees AS e
+JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+JOIN departments AS d
+ON de.dept_no = d.dept_no
+JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no
+WHERE dm.to_date = '9999-01-01'
+ORDER BY 'Manager Name';
+
+JOIN departments AS d
+ON d.em
 
 /* Joins only rows that have values in the mentioned keys */
 SELECT departments.dept_name AS dept_name, dept_manager.dept_no AS dept_no
