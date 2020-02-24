64e10a99a52d92f02ae0ce98f6a13a173f9db52d
William Stephens <william.a.stephens17@gmail.com>
2020-02-24 09:31:31 -0600

    JOIN

--- join_exercises.sql
+++ join_exercises.sql
@@ -24,6 +24,7 @@
 
 USE employees;
 
+/* Show each department along with the name of the current manager for that department. */
 SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', e.emp_no
 FROM departments AS d
 JOIN dept_manager AS dm
@@ -33,6 +34,7 @@ ON e.emp_no = dm.emp_no
 WHERE dm.to_date = '9999-01-01'
 ORDER BY d.dept_name;
 
+/* Find the name of all departments currently managed by women. */
 SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', e.emp_no
 FROM departments AS d
 JOIN dept_manager AS dm
@@ -42,9 +44,20 @@ ON e.emp_no = dm.emp_no
 WHERE dm.to_date = '9999-01-01' AND e.gender = 'F'
 ORDER BY d.dept_name;
 
+/* Find the current titles of employees currently working in the Customer Service department. */
+SELECT t.title AS Title, count(t.title)
+FROM employees AS e
+JOIN titles AS t
+ON e.emp_no = t.emp_no
+JOIN dept_emp AS de
+ON t.emp_no = de.emp_no
+JOIN departments AS d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Customer Service'
+GROUP BY t.title;
 
-#
-# SELECT * FROM employees WHERE emp_no = 110039;
+
+SELECT * FROM employees WHERE emp_no = 110039;
 
 /* Joins only rows that have values in the mentioned keys */
 SELECT departments.dept_name AS dept_name, dept_manager.dept_no AS dept_no
